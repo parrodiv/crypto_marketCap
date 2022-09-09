0581ca0681142739ee48c73657c128df3a9889a5
Alessandro Parrilla <paggio96@gmail.com>
2022-09-09 17:32:27 +0200

    Impostato Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Routes, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
 
-function App() {
+import { Navbar, Home, Cryptocurrencies, News, CryptoDetails } from './components'
+import './App.css'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route
+                exact
+                path='/cryptocurrencies'
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path='/crypto/:coinId' element={<CryptoDetails />} />
+              <Route exact path='/news' element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className='footer'>
+          <Typography.Title
+            level={5}
+            style={{ color: 'white', textAlign: 'center' }}
+          >
+            CryptoMarketCap <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

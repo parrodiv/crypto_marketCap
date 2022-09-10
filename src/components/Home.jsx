ee645e5be6b4e43deb519b72b1be65c474baf7ca
Alessandro Parrilla <paggio96@gmail.com>
2022-09-10 10:35:10 +0200

    Estratto lista cryptocurrencies e inserita nella pagina Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,8 @@ import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
 
 import { useGetCryptosQuery } from '../services/cryptoApi'
+import Cryptocurrencies from './Cryptocurrencies'
+import News from './News'
 
 const { Title } = Typography
 
@@ -29,6 +31,13 @@ const Home = () => {
       <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
       <Title level={2} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
     </div>
+    <Cryptocurrencies simplified/>
+    <div className="home-heading-container">
+      <Title level={2} className="home-title">Latest Crypto News</Title>
+      <Title level={2} className="show-more"><Link to="/news">Show more</Link></Title>
+    </div>
+    <News simplified/>
+    
   </>
 }
 
